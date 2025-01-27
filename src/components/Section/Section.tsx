61f8909dfe6a9d958bafdb3a3f8bb1a675732947
Oliver Booty <olboots@gmail.com>
2025-01-27 21:31:23 +0000

    Prevent reflowing of page when typing animation removes text

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -6,7 +6,7 @@ interface SectionProps extends React.HTMLAttributes<HTMLDivElement> {
 export const Section = ({ children, heading, ...props }: SectionProps) => {
     return (
         <section {...props}>
-            <h2 className="text-2xl md:text-3xl text-gray-600 dark:text-gray-400 mb-8">
+            <h2 className="text-2xl md:text-3xl min-h-[2.5rem] text-gray-600 dark:text-gray-400 mb-8">
                 {heading}
             </h2>
 
